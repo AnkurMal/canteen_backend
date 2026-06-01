0e9f205d47c40121fd730ca601d93db270068a9b
Ankur Mallick <ankurmallick80@gmail.com>
2026-06-01 17:47:46 +0530

    fix registration for user

--- user.py
+++ user.py
@@ -1,5 +1,5 @@
-from fastapi import APIRouter
-from sqlmodel import Field, SQLModel
+from fastapi import APIRouter, HTTPException, status
+from sqlmodel import Field, SQLModel, select
 
 from database import SessionDep, password_hash
 
@@ -25,7 +25,13 @@ router = APIRouter(prefix="/users", tags=["users"])
 
 
 @router.post("/", response_model=UserPublic)
-async def create_user(user: UserCreate, session: SessionDep):
+async def register_user(user: UserCreate, session: SessionDep):
+    user_exits = session.exec(
+        select(User).where(User.username == user.username)
+    ).first()
+    if user_exits is not None:
+        raise HTTPException(status.HTTP_409_CONFLICT, "Username is already taken")
+
     hashed = password_hash.hash(user.password)
     user_db = User(username=user.username, hashed_password=hashed)
 
